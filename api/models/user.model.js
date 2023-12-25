893e0ea94055616d15dc5678f2b23d24992b2095
Hanzy-ops <148455005+Hanzy-ops@users.noreply.github.com>
2023-12-26 05:52:09 +0700

    Add files via upload

---
+++ api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        default: "https://as2.ftcdn.net/v2/jpg/04/10/43/77/1000_F_410437733_hdq4Q3QOH9uwh0mcqAhRFzOKfrCR24Ta.jpg"
+        },
+},
+{timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
